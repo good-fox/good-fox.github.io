226fefb213331c6b300b5b83c4a05bbc4fdbc807
Vadym Skoroplias <skoroplias.official@gmail.com>
2020-02-13 00:19:18 +0200

    Add menu

--- game.js
+++ game.js
@@ -110,3 +110,29 @@ function f1() {
 
 //var gameOwer = document.getElementById('gameText');
 //gameOwer.innerHTML = 'Game lvl: ' + lvl;
+
+
+
+document.getElementById('nav').onmouseover = function (event) {
+    var target = event.target;
+    if (target.className == 'item'){
+        var s = target.getElementsByClassName('submenu');
+        closeMenu();
+        s[0].style.display = 'block';
+    }
+}
+
+document.onmouseover = function (event) {
+    var target = event.target;
+    if (target.className != 'item' && target.className != 'submenu'){
+        closeMenu();
+    }
+}
+
+function closeMenu() {
+    var menu = document.getElementById('nav');
+    var subm = document.getElementsByClassName('submenu');
+    for (var i = 0; i < subm.length; i++){
+        subm[i].style.display = 'none';
+    }
+}
